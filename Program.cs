e3df3e6072d6b30dfb234b5452e82469498578ca
“Konstantin <89112455963a@gmail.com>
2023-03-21 21:26:15 +0100

    Add code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GetCutArray(string[] array)
+{
+    int thirdsCharItems = 0;
+    int newArraySize = 0;
+    string[] newArray = new string[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArraySize++;
+            Array.Resize(ref newArray, newArraySize);
+            newArray[thirdsCharItems] = array[i];
+            thirdsCharItems++;
+        }
+    }
+    return newArray;
+}
+
+
+void ShowArray(object[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+string[] testArray = { "Hello", "2", "World", ":-)" };
+ShowArray(GetCutArray(testArray));
+
